14a1609f1745bdc0a0a00dfacde50677305f5017
Philippe "BooK" Bruhat <book@cpan.org>
2005-03-06 18:30:38 +0000

    The any theme select a random theme from which return names
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@87 77f6eb63-2709-0410-a607-da1692a51919

---
+++ lib/Acme/MetaSyntactic/any.pm
@@ -0,0 +1,59 @@
+package Acme::MetaSyntactic::any;
+use strict;
+use List::Util 'shuffle';
+use Acme::MetaSyntactic ();
+
+sub import {
+    # export the function
+    my $callpkg = caller;
+    my $meta    = Acme::MetaSyntactic::any->new;
+    no strict 'refs';
+    *{"$callpkg\::metaany"} = sub { $meta->name( @_ ) };
+}
+
+sub name {
+    my $self  = shift;
+    my $theme =
+      ( shuffle( grep { $_ ne 'any' } Acme::MetaSyntactic->themes ) )[0];
+    Acme::MetaSyntactic::metaname( $theme, @_ );
+}
+
+sub new { bless {}, shift }
+
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::any - Items from any theme.
+
+=head1 DESCRIPTION
+
+This theme simply selects a theme at random from all available
+themes, and return names from it.
+
+=head1 METHODS
+
+=over 4
+
+=item new()
+
+Create a new instance.
+
+=item name( $count )
+
+Implement the name() method for this class.
+
+=back
+
+=head1 CONTRIBUTOR
+
+Philippe Bruhat, upon request of Sébastien Aperghis-Tramoni.
+
+Introduced in version 0.12, published on March 7, 2005.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>
+
+=cut
+
