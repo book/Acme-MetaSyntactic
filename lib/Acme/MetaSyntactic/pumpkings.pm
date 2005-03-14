08d6456583732c6fa520962f36ef07dd6c5927f4
Philippe "BooK" Bruhat <book@cpan.org>
2005-03-14 10:01:45 +0000

    Add the new theme file : pumpking
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@109 77f6eb63-2709-0410-a607-da1692a51919

---
+++ lib/Acme/MetaSyntactic/pumpkings.pm
@@ -0,0 +1,34 @@
+package Acme::MetaSyntactic::pumpking;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::pumpking - The pumpking theme
+
+=head1 DESCRIPTION
+
+This is the list of the Perl Pumpkings, as listed in perlhist(1).
+
+The names are the pumpkings PAUSE id (except for C<NI-S>, which was
+changed to C<NI_S>).
+
+=head1 CONTRIBUTOR
+
+Rafael Garcia-Suarez.
+
+Introduced in version 0.14, published on March 21, 2005.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+lwall andyd tomc cbail ni_s chips timb micb gsar gbarr
+jhi hvds mschwern rgarcia nwclark rclamp lbrocard
