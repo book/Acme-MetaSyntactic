d7217d3c50c4e1a6bb7a5e265619e06b14ce7ef4
Philippe "BooK" Bruhat <book@cpan.org>
2005-04-10 14:43:53 +0000

    A test module
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@175 77f6eb63-2709-0410-a607-da1692a51919

---
+++ t/Acme/MetaSyntactic/digits.pm
@@ -0,0 +1,28 @@
+package Acme::MetaSyntactic::digits;
+use Acme::MetaSyntactic::Locale;
+our @ISA = ( Acme::MetaSyntactic::Locale );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::digits - The numbers theme
+
+=head1 DESCRIPTION
+
+You can count on this module. Almost.
+
+=cut
+
+__DATA__
+# default
+en
+# names en
+zero one two three four five six seven eight nine
+# names fr
+zero un deux trois quatre cinq six sept huit neuf
+# names it
+zero uno due tre quattro cinque sei sette otto nove
+# names yi
+nul eyn tsvey dray fir finf zeks zibn akht nayn
+
