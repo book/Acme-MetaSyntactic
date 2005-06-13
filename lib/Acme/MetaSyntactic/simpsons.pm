3cba80ff6d84dffce2c61e465b5b098e6b310636
Philippe "BooK" Bruhat <book@cpan.org>
2005-06-13 22:49:31 +0000

    new theme, almost
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@243 77f6eb63-2709-0410-a607-da1692a51919

---
+++ lib/Acme/MetaSyntactic/simpsons.pm
@@ -0,0 +1,33 @@
+package Acme::MetaSyntactic::simpsons;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::simpsons - The Simpsons theme
+
+=head1 DESCRIPTION
+
+Characters from the Simpsons serial.
+
+=head1 CONTRIBUTOR
+
+Philippe "BooK" Bruhat.
+
+Introduced in version 0.04, published on January 15, 2005.
+Disappeared in version 0.12, published on March 6, 2005.
+Re-introduced in version 0.26, published on June 13, 2005.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+bart lisa marge homer maggie moe mr_burns itchy scratchy grampa ned snowball
+
