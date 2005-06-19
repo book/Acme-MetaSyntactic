a23989f1aa2427badd1eaff976e17efe55161334
Philippe "BooK" Bruhat <book@cpan.org>
2005-06-19 23:04:27 +0000

    A new theme: groo
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@246 77f6eb63-2709-0410-a607-da1692a51919

---
+++ lib/Acme/MetaSyntactic/groo.pm
@@ -0,0 +1,37 @@
+package Acme::MetaSyntactic::groo;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::groo - The Groo the Wanderer theme
+
+=head1 DESCRIPTION
+
+A list of characters from the I<Groo the Wanderer> long-running
+comic-book authored by Sergio Aragonès and Mark Evanier.
+
+If you are intersted in Groo, you should have a look at
+L<http://www.groo.com/>, L<http://www.povonline.com/> and
+L<http://www.sergioaragones.com/>.
+
+=head1 CONTRIBUTOR
+
+Philippe "BooK" Bruhat.
+
+Introduced in version 0.27, published on June 21, 2005.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Groo Rufferto Sage Chakaal Minstrel Grooella Arba Dakarba Arcadio
+Taranto Pal Drumm Mulch Scribe Weaver Grativo Granny_Groo Pipil_Khan
+The_Witch_of_Kaan Captain_Ahax
