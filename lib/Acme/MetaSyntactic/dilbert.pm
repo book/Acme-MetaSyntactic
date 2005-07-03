10593bc01d483dcec22b9e6883819ec3c2f97774
Philippe "BooK" Bruhat <book@cpan.org>
2005-07-03 16:10:37 +0000

    updated the theme with a new list
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@260 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic/dilbert.pm
+++ lib/Acme/MetaSyntactic/dilbert.pm
@@ -13,13 +13,18 @@ Acme::MetaSyntactic::dilbert - The Dilbert theme
 
 Characters from the Dilbert daily strip.
 
+The list (with details) is available here:
+L<http://www.triviaasylum.com/dilbert/diltriv.html>.
+
 =head1 CONTRIBUTOR
 
-Sébastien Aperghis-Tramoni.
+Original contributor: Sébastien Aperghis-Tramoni.
 
 Introduced in version 0.03, published on January 14, 2005.
 
 Duplicate removed in version 0.15, published on March 28, 2005.
+
+Updated with a brand new list in version 0.29, published on July 4, 2005.
 
 =head1 SEE ALSO
 
@@ -29,6 +34,31 @@ L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
 
 __DATA__
 # names
-dilbert dogbert catbert
-ratbert bob dawn rex liz alice asok wally tina carol
-mordac ted phil elbonia pointy_haired_boss
+al alice allen ann anne anne_l_retentive antina asok aunt_helen
+avery_wong bad_ed barry becky ben betty beverly big_boss big_ed bill
+bob bob_flabeau bob_weaselton bobby bobby_mcnewton bobby_noober boron
+bottleneck_bill brad bradley brenda brenda_utthead brent brian bruce
+bucky bud buff_bufferman camping_carl carl carlos carol cheryl chuck
+cliffy co_op_employee connie cyrus_the_virus dan dave dawn dee_alamo
+dilbert doctor_wolfington dogbert donald dorie ed eddy edfred edna
+edward_lester_mann eileen ellen ernie flossie floyd floyd_remora
+fred freshy_q gustav hammerhead_bob harold harry_middlepart helen
+holly_hollister incredulous_ed irene irv irv_klepfurd jack janet
+jennifer jenny_dworkin jim jimmy jittery_jeff john_smith johnson jose
+juan_delegator judy karl kay_and_clem_bovinski ken kronos kudos larry
+laura lauren laurie les lisa liz lola loopy loud_howard lulu mahoney
+mary matt medical_mel mel michael_t_suit mike millard_bullrush milt ming
+miss_cerberus miss_mulput miss_pennington mister_catbert mister_goodenrich
+mittens mo mom monty mordac mother_nature motivation_fairy mr_death
+mr_dork myron nardo neal_snow ned nelson nervous_ed norma norman
+parrot_man paul paul_ooshen paul_tergeist peeved_eve peri_noid
+pete_peters peter phil phil_de_cube phil_from_heck pigboy plywoodboss
+pointy_haired_carl pointy_haired_pete proxis queen_bee_of_marketing
+randy ratbert ray rex richard rick robert_roberts roboboss rocky ron
+ruebert rufus_t_skwerrel russell sally sam_grooper sharon son_of_a_boss
+sophie stan susan sven techno_bill technology_buddha ted ted_griffin
+the_boss tim timmy tina toby todd tom too_helpful_guy topper toxic_tom
+traylor uncle_albert uncle_max uncle_ned upholsterygeist virginia
+waldo wally walter wendel wendy will willy wilson wilt_gandhi winston
+world_s_smartest_garbage_man yergi yorgi yugi yvonne zenox zimbu zoltar
+
