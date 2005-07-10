2709e66fad45b375425085991d872446fcc09903
Philippe "BooK" Bruhat <book@cpan.org>
2005-07-10 17:39:46 +0000

    new theme
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@268 77f6eb63-2709-0410-a607-da1692a51919

---
+++ lib/Acme/MetaSyntactic/counting_rhyme.pm
@@ -0,0 +1,57 @@
+package Acme::MetaSyntactic::counting_rhyme;
+use strict;
+use Acme::MetaSyntactic::Locale;
+our @ISA = qw( Acme::MetaSyntactic::Locale );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::counting_rhyme - The counting rhyme theme
+
+=head1 DESCRIPTION
+
+Based on popular children counting rhymes, mostly used to decide roles
+in games (who'll be the wolf?)
+
+=head1 FULL VERSIONS
+
+=head2 English
+
+Eeny, meeny, miny, moe
+Catch a tiger by the toe
+If he hollers let him go,
+Eeny, meeny, miny, moe.
+
+=head2 French
+
+Am, stram, gram,
+Pique et pique et colégram
+Bourre, bourre et ratatam
+Am, stram, gram.
+
+=head1 CONTRIBUTOR
+
+Xavier Caron proposed the idea in French, and Paul-Christophe Varoutas
+provided the English version.
+
+Introduced in version 0.30, published on July 11, 2005.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# default
+en
+# names en 
+eenie meeny miny moe
+catch a tiger by the toe 
+if he holers let him go 
+# names fr
+am stram gram
+pique et colegram
+bourre ratatam
+ 
