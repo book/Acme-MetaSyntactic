b9638f88b094491dc5263d7edb575653880b6864
Philippe "BooK" Bruhat <book@cpan.org>
2005-09-25 20:20:49 +0000

    no warnings please
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@329 77f6eb63-2709-0410-a607-da1692a51919

--- t/NoLang.pm
+++ t/NoLang.pm
@@ -1,6 +1,9 @@
 package main;
 $INC{"Win32/Locale.pm"} = 1;
-*Win32::Locale::get_language = sub {''};
+{
+    local $^W = 0;
+    *Win32::Locale::get_language = sub { '' };
+}
 delete $ENV{LANGUAGE};
 delete $ENV{LANG};
 1;
