8e7cd9d60ed32d835da1d50b3ae860c98e1c07ac
Philippe "BooK" Bruhat <book@cpan.org>
2005-10-02 21:04:30 +0000

    new theme for 0.42
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@335 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic.pm
+++ lib/Acme/MetaSyntactic.pm
@@ -7,7 +7,7 @@ use File::Basename;
 use File::Spec;
 use File::Glob;
 
-our $VERSION = '0.41';
+our $VERSION = '0.42';
 
 # some class data
 our $Theme = 'foo'; # default theme
---
+++ lib/Acme/MetaSyntactic/discworld.pm
@@ -0,0 +1,39 @@
+package Acme::MetaSyntactic::discworld;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::discworld - The discworld theme
+
+=head1 DESCRIPTION
+
+This theme contains items from Terry Pratchett's Disc World series.
+
+=head1 CONTRIBUTOR
+
+Martin Vorländer.
+
+Introduced in version 0.42, published on October 3, 2005.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+rincewind twoflower death nanny_ogg granny_weatherwax magrat_garlick
+mustrum_ridcully ponder_stibbons windle_poons eric mort ysabell
+alberto_malich lord_vetinari greebo pteppic ptraci captain_vimes carrot
+detritus sergeant_colon nobby reg_shoe errol gaspode bel_shamharoth
+offler creosote conina nijel koomi ksandra granpone bravd weasel tethis
+bathys garhartra dibbler gaffer silverfish
+
+ankh_morpork the_broken_drum dunmanifestin wyrmberg nef sto_lat lancre
+cori_celesti tsort pseudopolis sto_helit klatch quirm
+
