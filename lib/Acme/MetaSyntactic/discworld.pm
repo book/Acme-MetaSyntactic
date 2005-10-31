eb0383a6ac300f436a8ce1f52d98ba401ebcbfc6
Philippe "BooK" Bruhat <book@cpan.org>
2005-10-31 09:15:22 +0000

    theme update by Jean Forget
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@361 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic/discworld.pm
+++ lib/Acme/MetaSyntactic/discworld.pm
@@ -7,17 +7,19 @@ __PACKAGE__->init();
 
 =head1 NAME
 
-Acme::MetaSyntactic::discworld - The discworld theme
+Acme::MetaSyntactic::discworld - The Discworld theme
 
 =head1 DESCRIPTION
 
-This theme contains items from Terry Pratchett's Disc World series.
+This theme contains items from Terry Pratchett's Discworld series.
 
 =head1 CONTRIBUTOR
 
 Martin Vorländer.
 
 Introduced in version 0.42, published on October 3, 2005.
+
+Updated by Jean Forget in version 0.47, published on November 7, 2005.
 
 =head1 SEE ALSO
 
@@ -27,13 +29,21 @@ L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
 
 __DATA__
 # names
+great_a_tuin berilia jerakeen tubul great_t_phon
 rincewind twoflower death nanny_ogg granny_weatherwax magrat_garlick
-mustrum_ridcully ponder_stibbons windle_poons eric mort ysabell
-alberto_malich lord_vetinari greebo pteppic ptraci captain_vimes carrot
+agnes_nitt king_verence jason_ogg shawn_ogg
+mustrum_ridcully ponder_stibbons windle_poons ipslore igneous_cutwell
+eric mort ysabell susan binky
+alberto_malich lord_vetinari leonardo_da_quirm lady_sybil_ramkin
+greebo pteppic ptraci 
+captain_vimes carrot angua littlebottom
 detritus sergeant_colon nobby reg_shoe errol gaspode bel_shamharoth
-offler creosote conina nijel koomi ksandra granpone bravd weasel tethis
+offler blind_io creosote conina nijel koomi ksandra granpone bravd weasel tethis
 bathys garhartra dibbler gaffer silverfish
+brutha vorbis casanunda william_de_worde
+cohen caleb_the_ripper old_vincent mad_hamish boy_willy
 
 ankh_morpork the_broken_drum dunmanifestin wyrmberg nef sto_lat lancre
 cori_celesti tsort pseudopolis sto_helit klatch quirm
+ramtops uberwald djelibeybi ephebe genua llamedos agatean_empire xxxx
 
