17d8b0238341e23aeb0c5c3d0cae22c9a890ba2b
Philippe "BooK" Bruhat <book@cpan.org>
2005-11-13 21:52:04 +0000

    another dilbert update
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@384 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic/dilbert.pm
+++ lib/Acme/MetaSyntactic/dilbert.pm
@@ -73,4 +73,4 @@ the_boss tim timmy tina toby todd tom too_helpful_guy topper toxic_tom
 traylor uncle_albert uncle_max uncle_ned upholsterygeist virginia
 waldo wally walter wendel wendy will willy wilson wilt_gandhi winston
 world_s_smartest_garbage_man yergi yorgi yugi yvonne zenox zimbu zoltar
-tex
+tex flashy petricia
