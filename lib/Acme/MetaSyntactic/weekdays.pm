9d4724903de1559e9ff39ba1667166a9190abd5d
Philippe "BooK" Bruhat <book@cpan.org>
2005-11-26 19:45:33 +0000

    new theme weekdays
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@404 77f6eb63-2709-0410-a607-da1692a51919

---
+++ lib/Acme/MetaSyntactic/weekdays.pm
@@ -0,0 +1,30 @@
+package Acme::MetaSyntactic::weekdays;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::weekdays - The days of the week theme
+
+=head1 DESCRIPTION
+
+Days of the week.
+
+=head1 CONTRIBUTOR
+
+Abigail
+
+Introduced in version 0.51, published on December 5, 2005.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+monday tuesday wednesday thursday friday saturday sunday
