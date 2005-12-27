6234ab582166463947ba01282ef2a35c522cdadf
Philippe "BooK" Bruhat <book@cpan.org>
2005-12-27 21:37:39 +0000

    weekdays now multilingual
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@446 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic/weekdays.pm
+++ lib/Acme/MetaSyntactic/weekdays.pm
@@ -1,7 +1,7 @@
 package Acme::MetaSyntactic::weekdays;
 use strict;
-use Acme::MetaSyntactic::List;
-our @ISA = qw( Acme::MetaSyntactic::List );
+use Acme::MetaSyntactic::Locale;
+our @ISA = qw( Acme::MetaSyntactic::Locale );
 __PACKAGE__->init();
 1;
 
@@ -11,20 +11,71 @@ Acme::MetaSyntactic::weekdays - The days of the week theme
 
 =head1 DESCRIPTION
 
-Days of the week.
+Days of the week, in various languages.
+(Some letters have been brutally transliterated to latin-1.)
 
-=head1 CONTRIBUTOR
+See L<http://www.lexilogos.com/calendrier_jours.htm>, for
+many other languages.
 
-Abigail
+=head1 CONTRIBUTORS
+
+Abigail (English).
+
+Philippe "BooK" Bruhat (all the others languages, thanks to Estelle
+Souche for her help with the Yiddish names).
 
 Introduced in version 0.51, published on December 5, 2005.
 
+Made multilingual in version 0.55, published on January 2, 2006.
+
 =head1 SEE ALSO
 
-L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::Locale>.
 
 =cut
 
 __DATA__
-# names
+# default
+en
+# names ca
+dilluns dimarts dimecres dijous divendres dissabte diumenge
+# names co
+luni marti marcuri ghjovi venneri sabbatu dumenica
+# names cy
+llun mawrth mercher iau gwener sadwrn sul
+# names de
+montag dienstag mittwoch donnerstag freitag samstag sonntag
+# names dk
+mandag tirsdag onsdag torsdag fredag lordag sondag
+# names et
+esmapaev teisipaev kolmapaev neljapaev reede laupaev puhapaev
+# names en
 monday tuesday wednesday thursday friday saturday sunday
+# names es
+lunes martes miercoles jueves viernes sabado domingo
+# names eu
+astelehena asteartea asteazkena osteguna ostirala larunbata igandea
+# names fi
+maanantai tiistai keskiviikko torstai perjantai lauantai sunnuntai
+# names fr
+lundi mardi mercredi jeudi vendredi samedi dimanche
+# names it
+lunedi martedi mercoledi giovedi venerdi sabato domenica
+# names la
+lunae martis mercurii jovis veneris saturni solis
+# names lv
+svetdiena pirmdiena otrdiena tresdiena ceturtdiena piektdiena sestdiena
+# names nl
+maandag dinsdag woensdag donderdag vrijdag zaterdag zondag
+# names pl
+poniedzialek wtorek sroda czwartek piatek sobota niedziela
+# names pt
+segunda_feira terca_feira quarta_feira quinta_feira sexta_feira sabado domingo
+# names se
+mandag tisdag onsdag torsdag fredag lordag sondag
+# names sw
+jumatatu jumanne jumatano alhamisi ijumaa jumamosi jumapili
+# names x-pataphysical
+lundi mardi mercredi jeudi vendredi samedi hunyadi dimanche
+# names yi
+montik dinstik mitvokh donershtik fraytik shabes zuntik
