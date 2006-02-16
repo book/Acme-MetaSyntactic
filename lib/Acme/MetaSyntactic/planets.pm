1bf147d83d8d4892d1ea6217673fe0cd895e9e72
Philippe "BooK" Bruhat <book@cpan.org>
2006-02-16 10:01:31 +0000

    new theme planets for 0.63
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@519 77f6eb63-2709-0410-a607-da1692a51919

---
+++ lib/Acme/MetaSyntactic/planets.pm
@@ -0,0 +1,34 @@
+package Acme::MetaSyntactic::planets;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::planets - The planets theme
+
+=head1 DESCRIPTION
+
+The nine planets of our solar system.
+
+The status of the newly discovered Kuiper belt object is still not
+determined (and therefore, not officially named), and hence, not
+classified as a planet.
+
+=head1 CONTRIBUTOR
+
+Abigail
+
+Introduced in version 0.63, published on February 27, 2006.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune Pluto
