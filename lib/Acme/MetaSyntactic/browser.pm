3f858f429ced4ed37cae4a8a8c6b91b89d760c44
Philippe "BooK" Bruhat <book@cpan.org>
2006-04-14 00:44:54 +0000

    closing rt ticket #18700
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@588 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic/browser.pm
+++ lib/Acme/MetaSyntactic/browser.pm
@@ -13,11 +13,17 @@ Acme::MetaSyntactic::browser - The "browser" theme
 
 Some famous web browsers.
 
+Evolt.org maintains a browser archive at L<http://browsers.evolt.org/>.
+The information in the archive was not used to create this list (but
+may be in the future).
+
 =head1 CONTRIBUTORS
 
 Philippe "BooK" Bruhat, Sébastien Aperghis-Tramoni.
 
 Introduced in version 0.05, published on January 16, 2005.
+
+Updated in version 0.70, published on April 17, 2006.
 
 =head1 SEE ALSO
 
@@ -28,4 +34,4 @@ L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
 __DATA__
 # names
 mozilla netscape msie mosaic links lynx w3m opera galeon konqueror safari
-camino dillo amaya arachne omniweb planetweb voyager
+camino dillo amaya arachne omniweb planetweb voyager seamonkey
