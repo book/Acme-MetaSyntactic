a4e66a283b9587e729c3dea41456037b8310f1af
Philippe "BooK" Bruhat <book@cpan.org>
2006-04-28 23:44:02 +0000

    update theme planets for 0.73
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@618 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic/planets.pm
+++ lib/Acme/MetaSyntactic/planets.pm
@@ -1,7 +1,7 @@
 package Acme::MetaSyntactic::planets;
 use strict;
-use Acme::MetaSyntactic::List;
-our @ISA = qw( Acme::MetaSyntactic::List );
+use Acme::MetaSyntactic::Locale;
+our @ISA = qw( Acme::MetaSyntactic::Locale );
 __PACKAGE__->init();
 1;
 
@@ -13,9 +13,9 @@ Acme::MetaSyntactic::planets - The planets theme
 
 The nine planets of our solar system.
 
-The status of the newly discovered Kuiper belt object is still not
-determined (and therefore, not officially named), and hence, not
-classified as a planet.
+The status of the newly discovered Kuiper belt object (2003 UB313) is
+still not determined (and therefore, not officially named), and hence,
+not classified as a planet.
 
 =head1 CONTRIBUTOR
 
@@ -23,12 +23,26 @@ Abigail
 
 Introduced in version 0.63, published on February 27, 2006.
 
+Made multilingual in version 0.73, published on May 8, 2006.
+
 =head1 SEE ALSO
 
-L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::Locale>.
 
 =cut
 
 __DATA__
-# names
+# default
+en
+# names en
 Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune Pluto
+# names fr
+Mercure Venus Terre Mars Jupiter Saturne Uranus Neptune Pluton
+# names de
+Merkur Venus Erde Mars Jupiter Saturn Uranus Neptun Pluto
+# names it
+Mercurio Venere Terra Marte Giove Uranio Nettuno Plutone
+# names pt
+Mercurio Venus Terre Marte Jupiter Urano Netuno Plutao
+# names es
+Mercurio Venus Tierra Marte Jupiter Urano Neptuno Pluton
