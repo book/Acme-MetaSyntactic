5dd7df89d6c4468e014d46bda4416141adf09992
Philippe "BooK" Bruhat <book@cpan.org>
2006-05-15 08:01:52 +0000

    new theme norse_mythology
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@647 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic.pm
+++ lib/Acme/MetaSyntactic.pm
@@ -7,7 +7,7 @@ use File::Basename;
 use File::Spec;
 use File::Glob;
 
-our $VERSION = '0.74';
+our $VERSION = '0.75';
 
 # some class data
 our $Theme = 'foo'; # default theme
---
+++ lib/Acme/MetaSyntactic/norse_mythology.pm
@@ -0,0 +1,60 @@
+package Acme::MetaSyntactic::norse_mythology;
+use strict;
+use Acme::MetaSyntactic::MultiList;
+our @ISA = qw( Acme::MetaSyntactic::MultiList );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::norse_mythology - Characters from Norse mythology.
+
+=head1 DESCRIPTION
+
+A selection of characters from Norse mythology.
+
+=head1 CONTRIBUTOR
+
+Abigail
+
+Introduced in version 0.75, published on May 22, 2006.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::MultiList>.
+
+=cut
+
+__DATA__
+# default
+gods
+# names dwarves
+Ai Alf Althjof An Andvari Aurvang Austri Bifur Bofur Bombur Dain
+Dolgthrasir Dori Draupnir Duf Durin Dvalin Eikinskjaldi Fili Fith Fjalar
+Frar Frosti Fraeg Fundin Gandalf Ginnar Gloin Hannar Haugspori Heptifili
+Hlevang Hor Hornbori Jari Kili Lit Lofar Loni Mjothvitnir Motsognir
+Nain Nali Nar Niping Nithi Nori Northri Nyi Nyr Nyrath Onar Ori Rathvith
+Regin Skafith Skirfir Suthri Sviur Thekk Thorin Thrain Thror Vestri Vigg
+Vindalf Virfir Vit Yngvi
+# names giants
+AEgir Baugi Bergelmir Bestla Bolthorn Geirrod Gerd Gilling Gjalp Greip
+Gunnlod Gymir Hrod Hrungnir Hymir Ivaldi Jarnsaxa Kari Narve Loki
+Olvaldi Saxa Skadi Surtur Suttung Thiazi Thrudgelmir Thrym Utgardaloki
+Vafthrudnir Ymir
+# names gods
+AEgir Andhrimnir Aurvandil Balder Borr Bragi Buri Dagr Delling Eir
+Elli Forseti Freyja Freyr Frigg Fulla Gefjun Hel Heimdall Hermodr
+Hlin Hodr Hoenir Huldra Idunn Jord Kvasir Laga Lofn Loki Magni
+Mani Miming Mimir Modi Nanna Nerthus Njord Nott Odin Ottar Ran
+Saga Sif Sigyn Sjofn Snotra Sol Thor Thrud Tyr Ullr Vali Var Ve
+Vidar Vili Vor
+# names valkyries
+Brynhildr Geirahod Geiravor Geirdriful Geirskogul Geironul Godthjod Gunnr
+Gudr Goll Gondul Herfjotur Herja Hildr Hjalmthrimul Hjorthrimul Hlokk Hrist
+Hrund Mist Randgnid Randgridr Reginleif Radgridr Sanngridr Sigrdrifa
+Sigrun Skalmold Skeggold Skuld Skogul Sveid Svipul Svava THrima THrudr THogn
+# names worlds
+Alfheim Asgard Vanaheim
+Jotunheimr Midgardr Nidavellir
+Helheim Muspelheim Niflheim
+Yggdrasil
