2637fdbc659297be380019b3f1c8ac2397bd0d9a
Philippe "BooK" Bruhat <book@cpan.org>
2006-07-07 15:51:06 +0000

    Grativo => Gravito
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@695 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic/groo.pm
+++ lib/Acme/MetaSyntactic/groo.pm
@@ -24,6 +24,8 @@ Philippe "BooK" Bruhat.
 
 Introduced in version 0.27, published on June 21, 2005.
 
+Corrected a typo in version 0.82, published on July 10, 2006.
+
 =head1 SEE ALSO
 
 L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
@@ -33,5 +35,5 @@ L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
 __DATA__
 # names
 Groo Rufferto Sage Chakaal Minstrel Grooella Arba Dakarba Arcadio
-Taranto Pal Drumm Mulch Scribe Weaver Grativo Granny_Groo Pipil_Khan
+Taranto Pal Drumm Mulch Scribe Weaver Gravito Granny_Groo Pipil_Khan
 The_Witch_of_Kaan Captain_Ahax
