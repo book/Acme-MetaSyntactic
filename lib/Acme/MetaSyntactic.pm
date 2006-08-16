59b67ba05f609f25ae3c2b865c3eccb0a44ae335
Philippe "BooK" Bruhat <book@cpan.org>
2006-08-16 23:04:01 +0000

    David H. Adler opened ticket #21028
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@744 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic.pm
+++ lib/Acme/MetaSyntactic.pm
@@ -7,7 +7,7 @@ use File::Basename;
 use File::Spec;
 use File::Glob;
 
-our $VERSION = '0.87';
+our $VERSION = '0.88';
 
 # some class data
 our $Theme = 'foo'; # default theme
@@ -427,7 +427,7 @@ C<Acme::MetaSyntactic::Locale>), or a new one of your invention).
 
 Individual contributors are listed in the individual theme files.
 Look at the included F<CONTRIBUTORS> file for the list of all
-contributors (39 in this version).
+contributors (40 in this version).
 
 However, this module could not have been possible without:
 
