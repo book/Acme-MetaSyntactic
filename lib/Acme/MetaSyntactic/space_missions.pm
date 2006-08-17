bb377eb5afe6bfc910459c90fefed19013461240
Philippe "BooK" Bruhat <book@cpan.org>
2006-08-17 07:31:37 +0000

    New categories: manned_spacecraft, launch_vehicles and victims
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@751 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic/space_missions.pm
+++ lib/Acme/MetaSyntactic/space_missions.pm
@@ -3,7 +3,10 @@ use strict;
 use Acme::MetaSyntactic::MultiList;
 our @ISA = qw( Acme::MetaSyntactic::MultiList );
 __PACKAGE__->init();
-1;
+
+# And the magic true value to end the module
+# is the year of the first manned space mission
+1961;
 
 =head1 NAME
 
@@ -30,6 +33,33 @@ was grounded for health reasons.
 
 Source: I<The Right Stuff>, by Tom Wolfe.
 
+=item Manned Spacecraft
+
+This list gives the names of the manned spacecraft,
+all nations and all agencies or firms combined.
+
+=item Launch Vehicles
+
+This list gives the names of launch vehicles type. For vehicles with
+several numbered subtypes, only the main type has been given, without
+the subtype number (which gives one-letter names for Japanese launch
+vehicles).
+
+Source: The list of launch vehicles is taken from the colour
+photos from I<Rocket Science>, Alfred J. Zaehringer, Apogee
+(ISBN 1-894959-09-4).
+To this book, I have added
+L<http://en.wikipedia.org/wiki/List_of_launch_vehicles> and
+L<http://en.wikipedia.org/wiki/Scaled_Composites_White_Knight>.
+
+=item Victims
+
+This list gives the names of the humans who were killed in a
+spacecraft accident.
+
+Source: L<http://en.wikipedia.org/wiki/Category:Space_program_fatalities>
+L<http://en.wikipedia.org/wiki/Space_disaster>.
+
 =back
 
 =head1 CONTRIBUTOR
@@ -40,6 +70,9 @@ Introduced in version 0.41 as C<apollo>, published on September 26, 2005.
 
 Augmented with other space missions and renamed C<space_missions> in
 version 0.86, published on August 7, 2006.
+
+Updated with themes C<manned_spacecraft>, C<launch_vehicles> and C<victims>
+in version 0.88, published on August 21, 2006.
 
 =head1 SEE ALSO
 
@@ -68,3 +101,25 @@ Delta7
 Aurora7
 Sigma7
 Faith7
+# names manned_spacecraft
+Vostok Mercury X_15 Voskhod Gemini Soyuz
+Apollo Salyut Skylab Space_Shuttle
+Mir ISS Shenzhou SpaceShipOne
+# names launch_vehicles
+Vanguard Jupiter Thor Juno Redstone Atlas Centaur Agena Titan Delta Saturn Scout Pegasus Minotaur
+R_7 Cosmos N_1 Semyorka Proton Dnepr Energia Start
+Diamant Veronique
+Ariane
+Long_March
+Shavit
+N H J
+White_Knight
+# names victims
+Gus_Grissom Ed_White Roger_Chaffee
+Vladimir_Komarov
+Mike_Adams
+Georgi_Dobrovolski Viktor_Patsayev Vladislav_Volkov
+Greg_Jarvis Christa_McAuliffe Ronald_McNair Ellison_Onizuka Judith_Resnik Michael_J_Smith Dick_Scobee
+Rick_D_Husband William_McCool Michael_P_Anderson David_M_Brown
+Kalpana_Chawla Laurel_B_Clark Ilan_Ramon
+
