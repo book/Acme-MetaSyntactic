1c416175093433277eca1e2e3b7950c44cbc7ff2
Philippe "BooK" Bruhat <book@cpan.org>
2006-09-10 19:57:17 +0000

    the dutch list, and nato -> x-nato (extension)
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@800 77f6eb63-2709-0410-a607-da1692a51919

--- lib/Acme/MetaSyntactic/phonetic.pm
+++ lib/Acme/MetaSyntactic/phonetic.pm
@@ -30,7 +30,10 @@ Updated to handle multilingual phonetics in version 0.38,
 published on September 5, 2005.
 
 Updated (German fix by Gisbert W. Selke) in version 0.74, published on
-May 15, 2005.
+May 15, 2006.
+
+Updated with the Dutch list by Abigail in version 0.91, published on
+September 11, 2006.
 
 =head1 SEE ALSO
 
@@ -40,8 +43,8 @@ L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::Locale>.
 
 __DATA__
 # default
-nato
-# names nato
+x-nato
+# names x-nato
 alpha   bravo charlie  delta echo foxtrot golf  hotel  india juliet  kilo
 lima    mike  november oscar papa quebec  romeo sierra tango uniform victor
 whiskey xray  yankee   zulu
@@ -65,4 +68,9 @@ Siegfried Schule Theodor Ulrich Viktor  Wilhelm   Xanthippe Ypsilon  Zeppelin
 Ancona Bologna Como    Domodossola Empoli Firenze  Genova Hacca        Imola
 Jolly  Kappa   Livorno Milano      Napoli Otranto  Pisa   Quartomiglio Roma
 Savona Torino  Udine   Venezia     Wagner Xilofono York   Zara
+# names nl
+Anna    Anton    Bernard Cornelis Dirk     Eduard   Ferdinand Gerard
+Hendrik Izaak    Jan     Karel    Lodewijk Marie    Nico      Otto
+Pieter  Quotient Rudolf  Simon    Teunis   Theodoor Utrecht   Victor
+Willem  Xantippe Ypsilon IJsbrand Zaandam
 
