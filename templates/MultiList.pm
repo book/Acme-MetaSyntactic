f94656ca389d29572ec11e76fcac1e8787574873
Philippe "BooK" Bruhat <book@cpan.org>
2006-09-10 22:17:37 +0000

    some more templates
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@809 77f6eb63-2709-0410-a607-da1692a51919

---
+++ templates/MultiList.pm
@@ -0,0 +1,37 @@
+package Acme::MetaSyntactic::XXX;
+use strict;
+use Acme::MetaSyntactic::MultiList;
+our @ISA = qw( Acme::MetaSyntactic::MultiList );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::XXX - The XXX theme
+
+=head1 DESCRIPTION
+
+FIXME: Add a description here
+FIXME: Replace XXX with your theme name
+FIXME: Replace FIXME with the version number and the date
+FIXME: Add a default category
+
+=head1 CONTRIBUTOR
+
+FIXME
+
+Introduced in version FIXME, published on FIXME YYYY/MM/AA.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::MultiList>.
+
+=cut
+
+__DATA__
+# default
+FIXME: default category (:all means all items)
+# names category subcategory1
+
+# names category subcategory2
+
