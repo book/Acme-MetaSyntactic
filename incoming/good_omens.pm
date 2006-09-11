6f7d495e24c31ca36a016481a47aafc49762549b
Philippe "BooK" Bruhat <book@cpan.org>
2006-09-11 00:34:59 +0000

    upcoming theme for 0.97
    git-svn-id: svn://svn.mongueurs.net/Acme-MetaSyntactic/trunk@817 77f6eb63-2709-0410-a607-da1692a51919

---
+++ incoming/good_omens.pm
@@ -0,0 +1,59 @@
+package Acme::MetaSyntactic::good_omens;
+use strict;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+1;
+
+=head1 NAME
+
+Acme::MetaSyntactic::good_omens - The Good Omens theme
+
+=head1 DESCRIPTION
+
+This list gives the names of the I<Dramatis Personae> from
+Neil Gaiman and Terry Pratchett's novel, I<Good Omens>.
+
+Source: I<Good Omens>
+
+=head1 CONTRIBUTOR
+
+Jean Forget.
+
+Introduced in version 0.97, published on October 23, 2006.
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+God
+Metatron
+Aziraphale
+Satan
+Beelzebul
+Hastur
+Ligur
+Crowley
+War
+Famine
+Pollution
+Death
+Thou_Shalt_Not_Commit_Adultery_Pulsifer
+Agnes_Nutter
+Newton_Pulsifer
+Anathema_Device
+Shadwell
+Madame_Tracy
+Sister_Mary_Loquacious
+Mr_Young
+Mr_Tyler
+a_delivery_man
+Adam
+Pepper
+Wensleydale
+Brian
+Dog
